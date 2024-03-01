3a42c5f7edc4903cb77899630e84925912a8368e
Roar Emaus <roar@emaus.no>
2024-03-01 17:41:07 +0100

    first setup with dev run

--- buildah_dev.sh
+++ buildah_dev.sh
@@ -2,12 +2,13 @@
 set -x
 set -e
 
-bc=$(buildah from docker.io/library/alpine:latest)
-buildah run $bc apk add --no-cache git python3 py3-pip
-buildah run $bc pip3 install uvicorn[standard] fastapi pydantic gunicorn
+bc=$(buildah from docker.io/library/python:slim)
 
 buildah config --workingdir=/bjorlileika_api/ $bc
-buildah config --env DATA_PATH=/bjorlileika_api/data/ $bc
+buildah config --env SQLITE_DB_PATH=/bjorlileika_api/data/ $bc
+
+buildah copy $bc requirements.txt /tmp/requirements.txt
+buildah run $bc pip install -r /tmp/requirements.txt
 
 buildah commit $bc localhost/bjorlileika_api_dev:latest
 buildah rm $bc
---
+++ dev_run.sh
@@ -0,0 +1,1 @@
+podman run -p 8000:8000 -v $(pwd):/bjorlileika_api/ localhost/bjorlileika_api_dev:latest python api.py 
